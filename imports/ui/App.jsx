f0ff42633228ac98a27e72782ff11e208882cfe8
IsmaelPereira <ismaelfgpereira@gmail.com>
2021-03-15 19:05:40 -0300

    following the tutorial

---
+++ imports/ui/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./Task";
+import { TaskForm } from "./TaskForm";
+
+const tasks = [
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
+];
+
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    },
+  });
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
+export const App = () => {
+  const tasks = useTracker(() =>
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
+
+      <ul>
+        {tasks.map((task) => (
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
